7c367bd9bec481cfb10f69ee6aa138daa2d99da1
bevinh <bevin.hernandez@gmail.com>
2017-01-15 21:44:03 -0500

    Rearranged app, added navigation and the header

---
+++ app.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app = express();
+const pug = require('pug');
+
+//Set the view engine and the template lookup
+app.set('view engine', 'pug');
+app.set('views', __dirname + '/src/templates');
+//Set the places to look for the static files
+app.use('/static', express.static(__dirname + '/src/public'));
+
+app.get('/', function(req, res){
+    var path = req.path;
+    res.locals.path = path;
+    res.render('index');
+
+});
+
+app.listen(3000, function () {
+    console.log('You got an app listening on port 3000!')
+})
--- js/app.js
+++
@@ -1,7 +0,0 @@
-var express = require('express');
-var app = express();
-const pug = require('pug');
-
-app.listen(3000, function () {
-    console.log('You got an app listening on port 3000!')
-})
