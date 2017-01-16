00e6a531f6e1d16191a7fc24ae3aebea99a72f59
bevinh <bevin.hernandez@gmail.com>
2017-01-16 09:11:32 -0500

    Added initial twitter retrieval, added config file

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
-var express = require('express');
-var app = express();
-var tweets = require('./mocks/tweets.json');
+var express = require('express'),
+    app = express(),
+    tweets = require('./mocks/tweets.json'),
+    twit = require('twit'),
+    config = require('./config');
 const pug = require('pug');
 
 //Set the view engine and the template lookup
@@ -9,11 +11,20 @@ app.set('views', __dirname + '/templates');
 //Set the places to look for the static files
 app.use('/static', express.static(__dirname + '/public'));
 
+var twitty = new twit({
+    consumer_key:         config.consumer_key,
+    consumer_secret:      config.consumer_secret,
+    access_token:         config.access_token,
+    access_token_secret:  config.access_token_secret,
+    timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+});
 //add the first page
 app.get('/', function(req, res){
+    twitty.get('followers/list', { screen_name: 'bevinhernandez' },  function (err, data, response) {
+        console.log(data);
+    });
     var path = req.path;
-    var screenName = tweets[0].user.screen_name;
-    console.log(tweets[0].user.screen_name);
+    var screenName = 'bevinhernandez';
     res.locals.path = path;
     res.render('index', {screenName: screenName });
 
