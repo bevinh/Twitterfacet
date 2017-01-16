9cc68d04888f0545c5248cd60529cf38b258025d
bevinh <bevin.hernandez@gmail.com>
2017-01-16 08:45:19 -0500

    Added start script, and added the screen name from the JSON file to the nav

--- src/app.js
+++ src/app.js
@@ -1,19 +1,21 @@
 var express = require('express');
 var app = express();
-var tweets = require('/src/mocks/tweets.json');
+var tweets = require('./mocks/tweets.json');
 const pug = require('pug');
 
 //Set the view engine and the template lookup
 app.set('view engine', 'pug');
-app.set('views', __dirname + '/src/templates');
+app.set('views', __dirname + '/templates');
 //Set the places to look for the static files
-app.use('/static', express.static(__dirname + '/src/public'));
+app.use('/static', express.static(__dirname + '/public'));
 
 //add the first page
 app.get('/', function(req, res){
     var path = req.path;
+    var screenName = tweets[0].user.screen_name;
+    console.log(tweets[0].user.screen_name);
     res.locals.path = path;
-    res.render('index');
+    res.render('index', {screenName: screenName });
 
 });
 
